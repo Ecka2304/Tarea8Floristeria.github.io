78dfc16179eabbcbbd63fe697b3c53aa4c2883ba
Ericka <84459685+Ecka2304@users.noreply.github.com>
2024-03-28 19:39:24 -0600

    Add files via upload

---
+++ js/prov_cant_dist.js
@@ -0,0 +1,71 @@
+const $d = document;
+const $Provincias = $d.getElementById("prov");
+const $Cantones = $d.getElementById("can");
+const $Distritos = $d.getElementById("dis");
+
+function provincia() {
+    fetch("https://ubicaciones.paginasweb.cr/provincias.json")
+    .then(res => res.ok ? res.json() : Promise.reject(res))
+    .then(json => {
+        var $options = '<option value="">Selecciona una provincia</option>';
+        Object.values(json).forEach(provincia => {
+            $options += `<option value="${provincia}">${provincia}</option>`;
+        });
+        $Provincias.innerHTML = $options; 
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos de la API:', error);
+    });
+}
+
+function cargarCantones(provincia) {
+    fetch(`https://ubicaciones.paginasweb.cr/provincia/${provincia}/cantones.json`)
+    .then(res => res.ok ? res.json() : Promise.reject(res))
+    .then(json => {
+        var $options = '<option value="">Selecciona un cantón</option>';
+        Object.values(json).forEach(canton => {
+            $options += `<option value="${canton}">${canton}</option>`;
+        });
+        $Cantones.innerHTML = $options;
+    })
+    .catch(error => {
+        console.error('Error al obtener los cantones de la API:', error);
+    });
+}
+
+function cargarDistritos(provincia, canton) {
+    fetch(`https://ubicaciones.paginasweb.cr/provincia/${provincia}/canton/${canton}/distritos.json`)
+    .then(res => res.ok ? res.json() : Promise.reject(res))
+    .then(json => {
+        var $options = '<option value="">Selecciona un distrito</option>';
+        Object.values(json).forEach(distrito => {
+            $options += `<option value="${distrito}">${distrito}</option>`;
+        });
+        $Distritos.innerHTML = $options;
+    })
+    .catch(error => {
+        console.error('Error al obtener los distritos de la API:', error);
+    });
+}
+
+$Provincias.addEventListener('change', function() {
+    const provinciaSeleccionada = this.value;
+    if (provinciaSeleccionada !== "") {
+        cargarCantones(provinciaSeleccionada);
+    } else {
+        $Cantones.innerHTML = '<option value="">Selecciona un cantón</option>';
+        $Distritos.innerHTML = '<option value="">Selecciona un distrito</option>';
+    }
+});
+
+$Cantones.addEventListener('change', function() {
+    const provinciaSeleccionada = $Provincias.value;
+    const cantonSeleccionado = this.value;
+    if (cantonSeleccionado !== "") {
+        cargarDistritos(provinciaSeleccionada, cantonSeleccionado);
+    } else {
+        $Distritos.innerHTML = '<option value="">Selecciona un distrito</option>';
+    }
+});
+
+$d.addEventListener("DOMContentLoaded", provincia)
